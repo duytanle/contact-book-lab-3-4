e0c45a92b5089ecd9599c96d8e01a26439dbbe50
duytanle <leduytan1905@gmail.com>
2022-10-31 15:12:05 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "ContactBook",
     component: ContactBook,
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="page">
+    <p>
+        Oops, không thể tìm thấy trang. Trở về
+        <router-link to="/">trang chủ.</router-link>
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
